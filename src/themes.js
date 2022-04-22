b7ab7a92d8a8a0c4e275db444ff45d747945edf6
ernestberziu <eberziu1@gmail.com>
2022-04-22 03:20:45 +0200

    others

---
+++ src/themes.js
@@ -0,0 +1,16 @@
+export const themes = {
+  light: {
+    primaryColor: '#FFFFFF',
+    secondaryColor: '#F8F8FA',
+    textColor: '#12131B',
+    buttonPrimaryColor: '#1264A3',
+    buttonSecondaryColor: '#FFFFFF',
+  },
+  dark: {
+    primaryColor: '#12131B',
+    secondaryColor: '#1F212D',
+    textColor: '#FFFFFF',
+    buttonPrimaryColor: '#1264A3',
+    buttonSecondaryColor: '#FFFFFF',
+  },
+};
